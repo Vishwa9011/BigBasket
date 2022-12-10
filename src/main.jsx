69d74fb1345f11abdf2b1169f7803fd4b58e3836
Vishwa Vivek Yadav <112635764+Vishwa9011@users.noreply.github.com>
2022-12-10 14:50:13 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>
 )
